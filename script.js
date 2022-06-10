28826c10d098eea61c602c3ae9c7f07bac7c20aa
violanerd <madalyne.cross@gmail.com>
2022-06-10 13:10:01 -0500

    initial structure

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+//an empty object to be added to
+var passwordInfo = {
+}
+//functions to add to the passswordInfo object
+var passwordLength = function () { 
+  var num = window.prompt("How long would you like your password to be? Anywhere from 8 to 128 characters.");
+  if (num < 8 || num > 128) {
+    window.alert("Please choose an appropriate length password");
+    generatePassword();
+  } 
+  else {
+    window.alert("Great! Your password will be " + num + " characters long. Let's make some more choices about your password.");
+  } 
+  return num;
+}
+
+//generate the password
+function generatePassword () {
+  passwordInfo.length = passwordLength();
+  console.log(passwordInfo);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
