cce6819a3b2fcc749bad401483134aea39b60dee
violanerd <madalyne.cross@gmail.com>
2022-06-10 17:35:22 -0500

    work on character type prompts

--- script.js
+++ script.js
@@ -11,14 +11,68 @@ var passwordLength = function () {
     generatePassword();
   } 
   else {
-    window.alert("Great! Your password will be " + num + " characters long. Let's make some more choices about your password.");
+    window.alert("Great! Your password will be " + num + " characters long. Let's pick characters to add to your password.");
   } 
   return num;
+} 
+
+var passwordUpper = function () {
+  var upper = window.confirm("Would you like to use uppercase letters?");
+  if (upper) {
+    window.alert("Great, your password will have uppercase letters.");
+    return upper;
+  } else {
+    window.alert("Ok, no uppercase.");
+    return false;
+  }
 }
+var passwordLower = function () {
+  var lower = window.confirm("Would you like to use lowercase letters?");
+  if (lower) {
+    window.alert("Great, your password will have lowercase letters.");
+    return lower;
+  } else {
+    window.alert("Ok, no lowercase.");
+    return false;
+  }
+}
+var passwordNumeric = function () {
+  var numeric = window.confirm("Would you like to use numeric characters?");
+  if (numeric) {
+    window.alert("Great, your password will have numbers.");
+    return numeric;
+  } else {
+    window.alert("Ok, no numbers.");
+    return false;
+  }
+}
+var passwordSpecial = function () {
+  var special = window.confirm("Would you like to use special characters?");
+  if (special) {
+    window.alert("Great, your password will have special characters.");
+    return special;
+  } else if (passwordInfo.upper === false && passwordInfo.lower === false && passwordInfo.numeric === false) {
+    window.alert("You must pick at least one character: upper, lower, numeric or special.");
+    passwordUpper(); //this should be a function that redoes them, look at gladiator robots
+  } else {
+    window.alert("Ok, no special characters.");
+    return false;
+  }
+}
+
+// Will have to wrap the characters in a function, won't continually call down the list.
+// ??How to store the variable?
+
+
+
 
 //generate the password
 function generatePassword () {
   passwordInfo.length = passwordLength();
+  passwordInfo.upper = passwordUpper();
+  passwordInfo.lower = passwordLower();
+  passwordInfo.numeric = passwordNumeric();
+  passwordInfo.special = passwordSpecial(); 
   console.log(passwordInfo);
 }
 
